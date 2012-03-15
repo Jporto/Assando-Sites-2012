ff26a9c4a2ac7c84f03f5948290e2e0704bf4d64
Thiago Belem <contato@thiagobelem.net>
2012-03-15 00:55:41 -0300

    TestCase para todos os testes do projeto

---
+++ Test/Case/AllTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Executa todos os testes do projeto
+ */
+class AllTest extends CakeTestSuite {
+
+	public static function suite() {
+
+		$suite = new CakeTestSuite('All APP tests');
+		$suite->addTestDirectoryRecursive(TESTS . 'Case');
+
+		return $suite;
+	}
+
+}
